15b43346f94b9b5a44694b8c853654bee1248b52
StevenNguyenUIT <143907704+StevenNguyenUIT@users.noreply.github.com>
2024-04-12 18:59:04 +0700

    Initial commit

---
+++ app/src/main/java/com/nhinhnguyenuit/recyclerview08012014/Cinema.kt
@@ -0,0 +1,9 @@
+package com.nhinhnguyenuit.recyclerview08012014
+
+class Cinema (
+    var image: Int,
+    var name: String,
+    var address: String,
+    var phone: String,
+    var distance: Float
+)
